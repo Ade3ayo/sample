323032225e0acec4c0e184baa8c439737494b8c5
edjucatur <babzadelod>
2023-10-01 21:08:56 +0100

    another

--- index.js
+++ index.js
@@ -7,11 +7,36 @@ const form = document.querySelector("#form");
 const success = document.querySelector(".success");
 const inputValue = document.querySelector(".inputValue");
 const dismiss = document.querySelector(".dismiss");
+const invalid = document.querySelector(".invalid-msg");
+const input = document.querySelector("input");
 
-form.addEventListener("submit", function () {
-  wrapper.classList.add("hidden");
-  success.classList.remove("hidden");
-  inputValue.innerHTML = email.value + ".";
+function error() {
+  invalid.classList.remove("hidden");
+  input.style.color = "hsl(4, 100%, 67%)";
+  input.style.background = "pink";
+  input.style.border = "solid 0.5px hsl(4, 100%, 67%)";
+}
+
+email.addEventListener("input", function () {
+  if (email.validity.valid) {
+    invalid.classList.add("hidden");
+    input.style.background = "white";
+    input.style.color = "black";
+    input.style.border = "none";
+  } else {
+    error();
+  }
+});
+
+form.addEventListener("submit", function (e) {
+  if (!email.validity.valid) {
+    error();
+  } else {
+    wrapper.classList.add("hidden");
+    success.classList.remove("hidden");
+    inputValue.innerHTML = email.value + ".";
+  }
+  e.preventDefault();
 });
 
 dismiss.addEventListener("click", function () {
